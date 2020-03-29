6bf9535fc29c53590cc12cccd46399a93c7b6bae
Derrick Mei <derrick.mei.168@gmail.com>
2020-03-29 02:46:51 -0500

    refactored couth th using ternary

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,9 +8,11 @@ def count_th(word):
     if len(word) < 2:
         return 0
 
-    isTh = 0
+    # isTh = 0
 
-    if word[:2] == 'th':
-        isTh = 1
+    # if word[:2] == 'th':
+    #     isTh = 1
 
-    return isTh + count_th(word[1:])
+    # return isTh + count_th(word[1:])
+
+    return (1 if word[:2] == 'th' else 0) + count_th(word[1:])
